3dba6745082bc89c96c50619ae147af091ba80b2
Eduardo <eduardooliveira7907@gmail.com>
2025-11-15 09:29:40 -0300

    feat: finalizando teste 1 classe produto

--- vendas/src/main/java/br/icev/vendas/Produto.java
+++ vendas/src/main/java/br/icev/vendas/Produto.java
@@ -8,17 +8,40 @@ public class Produto {
     private final BigDecimal precoUnitario;
 
     public Produto(String codigo, String nome, BigDecimal precoUnitario) {
+        if (precoUnitario == null) {
+            throw new NullPointerException("Preço não pode ser igual a nulo");
+        }
+        if (precoUnitario.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Preço não pode ser negativo");
+        }
+
         this.codigo = codigo;
         this.nome = nome;
         this.precoUnitario = precoUnitario;
     }
 
-    public String getCodigo() { throw new UnsupportedOperationException("TODO"); }
-    public String getNome() { throw new UnsupportedOperationException("TODO"); }
-    public BigDecimal getPrecoUnitario() { throw new UnsupportedOperationException("TODO"); }
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
 
     @Override
-    public boolean equals(Object o) { throw new UnsupportedOperationException("TODO"); }
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+
+        Produto produto = (Produto) o;
+        return codigo.equals(produto.codigo);
+    }
     @Override
-    public int hashCode() { throw new UnsupportedOperationException("TODO"); }
+    public int hashCode() {
+        return codigo.hashCode();
+    }
 }
